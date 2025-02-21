8a38ae6a723d4e843464205a9e875cca3126fe05
Josed1054 <jl4ariza@hotmail.com>
2025-02-21 11:06:45 -0600

    feat(atm): add pin input to the screen

--- components/atm/screen/Screen.tsx
+++ components/atm/screen/Screen.tsx
@@ -1,5 +1,5 @@
+import ReactCodeInput from "react-verification-code-input";
 import clsx from "clsx";
-
 export enum LINE_SIDE {
   LEFT = "left",
   RIGHT = "right",
@@ -16,10 +16,16 @@ interface IATMScreenProps {
 
 export default function ATMScreen({ options }: Readonly<IATMScreenProps>) {
   return (
-    <div className="bg-[#7EB4D5] border-4 grid grid-cols-2 grid-rows-6 gap-3 border-[#E1E1D6] row-start-2 w-full h-54">
-      <div className="col-span-2 row-span-2 flex flex-col items-center justify-center gap-2">
+    <div className="bg-[#7EB4D5] border-4 grid grid-cols-2 grid-rows-6 gap-3 border-[#E1E1D6] row-start-2 w-full h-58">
+      <div className="col-span-2 row-span-2 flex flex-col items-center justify-center gap-1 p-4 w-full h-full">
         <p className="text-white text-sm">Welcome to the ATM</p>
         <p className="text-white text-xs">Please enter your PIN</p>
+        <ReactCodeInput
+          fields={4}
+          className="!w-auto [&_input]:bg-white [&_input]:!text-black [&_input]:!text-xs [&_input]:!font-mono"
+          fieldWidth={20}
+          fieldHeight={16}
+        />
       </div>
       {options.map((option) => (
         <div
