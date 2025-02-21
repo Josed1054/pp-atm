d4f3bfa0169b67ddbc2758936bd74c97ffce0ffa
Josed1054 <jl4ariza@hotmail.com>
2025-02-21 09:22:37 -0600

    feat(atm): buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,30 @@
+"use client";
+
+import ATMButtons from "@/components/atm/Buttons";
 import Image from "next/image";
+
+const LEFT_BUTTONS = [
+  {
+    label: "1",
+    onClick: () => {},
+    disabled: false,
+  },
+  {
+    label: "2",
+    onClick: () => {},
+    disabled: false,
+  },
+  {
+    label: "3",
+    onClick: () => {},
+    disabled: false,
+  },
+  {
+    label: "4",
+    onClick: () => {},
+    disabled: false,
+  },
+];
 
 export default function Home() {
   return (
@@ -27,20 +53,20 @@ export default function Home() {
         </div>
         <div className="bg-gray-300 w-11/12 h-2" />
         <div className="bg-[#F1F0E7] w-11/12 md:h-2/3 h-4/5 relative">
-          <div className="grid grid-cols-[auto_1fr_auto] w-full p-2 gap-2">
+          <div className="grid grid-cols-[auto_1fr_auto] w-full p-2 gap-0 gap-y-1">
             <Image
-              className="w-full col-start-2 h-6"
+              className="w-full col-start-2 h-8"
               src="/img/atm/creditcard_sprite.png"
               alt="credit card sprite"
               width={150}
               height={10}
             />
-            <div className="bg-gray-400 row-start-2 w-14 h-24 self-end" />
+            <ATMButtons direction="left" buttons={LEFT_BUTTONS} />
             <div
               aria-label="screen"
-              className="bg-[#7EB4D5] border-4 border-[#E1E1D6] row-start-2 w-full h-44"
+              className="bg-[#7EB4D5] border-4 border-[#E1E1D6] row-start-2 w-full h-54"
             />
-            <div className="bg-gray-400 row-start-2 w-14 h-24 self-end" />
+            <ATMButtons direction="right" buttons={LEFT_BUTTONS} />
             <Image
               className="w-10 col-start-2 row-start-3 h-1.5 justify-self-end"
               src="/img/atm/systems.png"
@@ -50,7 +76,7 @@ export default function Home() {
             />
           </div>
           <Image
-            className="w-36 col-start-2 h-auto absolute md:top-2/5 top-1/3 left-1/12"
+            className="w-36 col-start-2 h-auto absolute md:top-5/10 top-1/3 left-1/12"
             src="/img/atm/sticker_graf.png"
             alt="sticker graf"
             width={150}
---
+++ components/atm/Buttons.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { Button } from "../ui/button";
+import clsx from "clsx";
+
+interface IATMButtonsProps {
+  direction: "left" | "right";
+  buttons: {
+    label: string;
+    onClick: () => void;
+    disabled?: boolean;
+  }[];
+}
+
+export default function ATMButtons({
+  direction,
+  buttons,
+}: Readonly<IATMButtonsProps>) {
+  return (
+    <div
+      className={clsx(
+        "grid grid-rows-5 grid-cols-1 items-center gap-3 row-start-2 w-14 h-full border-y-4 border-transparent",
+        direction === "right" && "col-start-3"
+      )}
+    >
+      <div />
+      <div />
+      {buttons.map((button, index) => (
+        <div key={`${button.label}-${index}`} className="flex items-center">
+          {direction === "right" && <div className="w-1/4 h-1 bg-[#C1C1C1]" />}
+          <Button
+            className="w-3/4 h-6 bg-[#C1C1C1] rounded-md border-y-2 border-b-[#9B9B9B] border-t-[#D9D9D4] hover:bg-slate-300"
+            onClick={button.onClick}
+            disabled={button.disabled}
+          />
+          {direction === "left" && <div className="w-1/4 h-1 bg-[#C1C1C1]" />}
+        </div>
+      ))}
+    </div>
+  );
+}
